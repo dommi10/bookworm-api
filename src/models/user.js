d7c7de8e7a5687f84e899743678c9ab604e0f7cb
--help <domsbuhendwa2@gmail.com>
2020-04-29 08:48:49 +0200

    add confirmed field to token

--- src/models/user.js
+++ src/models/user.js
@@ -32,6 +32,7 @@ schema.methods.generateJWT = function generateJWT() {
   return jwt.sign(
     {
       email: this.email,
+      confirmed: this.confirmed,
     },
     process.env.JWT_SECRET
   );
