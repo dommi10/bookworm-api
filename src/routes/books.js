9e42d3c19089f3f17a0257f8b5d95f2d1f9f585d
--help <domsbuhendwa2@gmail.com>
2020-05-08 07:38:31 +0200

    refractor books and env

--- src/routes/books.js
+++ src/routes/books.js
@@ -9,10 +9,25 @@ route.use(authenticate);
 route.get("/search", (req, res) => {
   request
     .get(
-      `https://www.goodreads.com/search/index.xml?key=I89tI8gjlIgVufDTQT2rQ&q=${req.query.q}`
+      `https://www.goodreads.com/search/index.xml?key=${process.env.GOODREADS_KEY}&q=${req.query.q}`
     )
     .then((result) =>
-      parseString(result, (err, goodResult) => res.json({ goodResult }))
+      parseString(result, (err, goodResult) => {
+        if (err) {
+          res.status(403).json({ errors: { global: "something went wrong" } });
+        } else
+          res.json({
+            books: goodResult.GoodreadsResponse.search[0].results[0].work.map(
+              (work) => ({
+                goodreadsId: parseInt(work.best_book[0].id[0]._, 2),
+                title: work.best_book[0].title[0],
+                authors: work.best_book[0].author[0].name[0],
+                covers: [work.best_book[0].image_url[0]],
+                // pages: 198,
+              })
+            ),
+          });
+      })
     );
   // res.status(200).json({
   // books: [
