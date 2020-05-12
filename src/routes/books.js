70ef4747c9084d52b562bf3f9723c1f7b7c0a2ce
--help <domsbuhendwa2@gmail.com>
2020-05-12 06:43:52 +0200

    fix goodreadsid bug and add fetchpages

--- src/routes/books.js
+++ src/routes/books.js
@@ -15,17 +15,36 @@ route.get("/search", (req, res) => {
       parseString(result, (err, goodResult) => {
         if (err) {
           res.status(403).json({ errors: { global: "something went wrong" } });
-        } else
+        } else {
           res.json({
             books: goodResult.GoodreadsResponse.search[0].results[0].work.map(
               (work) => ({
-                goodreadsId: parseInt(work.best_book[0].id[0]._, 2),
+                goodreadsId: parseInt(work.best_book[0].id[0]._, 10),
                 title: work.best_book[0].title[0],
                 authors: work.best_book[0].author[0].name[0],
                 covers: [work.best_book[0].image_url[0]],
-                // pages: 198,
+              
               })
             ),
+          });
+        }
+      })
+    );
+});
+
+route.get("/fetchPages", (req, res) => {
+  const { goodreadsId } = req.query;
+  request
+    .get(
+      `https://www.goodreads.com/book/show.xml?key=${process.env.GOODREADS_KEY}&id=${goodreadsId}`
+    )
+    .then((result) =>
+      parseString(result, (err, goodResult) => {
+        if (err) {
+          res.status(403).json({ errors: { global: "something went wrong" } });
+        } else
+          res.json({
+            pages: goodResult.GoodreadsResponse.book[0].num_pages[0],
           });
       })
     );
